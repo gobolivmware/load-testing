7ffaadadfbae2ca3f838f7f9b3f5a9f1b727c176
Giovani Henrique Oboli <goboli@daitan.com>
2020-07-08 14:46:12 -0300

    setting stages for the load test

--- loadtest.js
+++ loadtest.js
@@ -1,7 +1,21 @@
-import http from 'k6/http';
-import { sleep } from 'k6';
+import http from "k6/http";
+import { check, sleep } from "k6";
 
-export default function() {
-  http.get('http://test.k6.io');
+// init code
+export let options = {
+  stages: [
+    { duration: "30s", target: 20 },
+    { duration: "10s", target: 10 },
+    { duration: "20s", target: 0 },
+  ],
+};
+
+// similar to the "main" function in Java,
+// the default entry for the Virtual Users, for the test start.
+export default function () {
+
+  // VIRTUAL USER CODE
+  let res = http.get("https://httpbin.org/");
+  check(res, { "status was 200": (r) => r.status == 200 });
   sleep(1);
-}
+};
